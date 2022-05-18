a33c45251fdfdcc42e6239dcdd467606e4d94fe9
Visionist <escaperecovery@gmail.com>
2022-05-18 05:57:33 +0000

    Updated UI

--- game.js
+++ game.js
@@ -5,19 +5,13 @@ function Game(channelId) {
   this.letters = "";
   this.turn = 0;//index of player of turn
   this.turnType = 0;//0: add letter or challenge, 1: response to challenge
+  this.totalTurns = 0;//keep track for timers
   this.lastPlayer; //the last player to take a turn
+  this.paused = false;//paused games ignore normal messages
 }
 
 Game.prototype.addLetter = function(player, letter) {
-  if (!this.checkInGame(player)) {
-    console.log(player.username + " is not in the game")
-    return "You aren't playing in this game.  Type l!join to join!"; //check if not in game
-  }
-  if (this.getPlayerIds()[this.turn] !== player.id) {
-    console.log("it is not "+player.username + " turn")
-    return "It is " + this.getPlayers()[this.turn]+"'s turn now."; //check if player's turn
-  }
-  
+  //add the letter
   if (letter.length == 1) {
     this.nextTurn();
     this.letters += letter.toLowerCase(); //add the single letter
@@ -28,25 +22,19 @@ Game.prototype.addLetter = function(player, letter) {
     this.letters += letter.toLowerCase().substring(letter.length-1); //add the last letter of complete word
     return "The word is now `"+this.letters+"`";//successful
   }
-  return "Please enter a letter.";//unsucecssful
+  //unsucecssful, not a letter
+  if (this.letters == "") return "Please enter a letter.  The word is currently empty";
+  else return "Please enter a letter.  The word is `"+this.letters+" `";
 }
 
 Game.prototype.challenge = function(player) {
-  if (!this.checkInGame(player)) {
-    console.log(player.username + " is not in the game")
-    return "You aren't playing in this game.  Type `l!join` to join!"; //check if not in game
-  }
-  if (this.getPlayerIds()[this.turn] !== player.id) {
-    console.log("it is not "+player.username + " turn")
-    return "It is " + player.username+"'s turn now."; //check if player's turn
-  }
   if (this.letters.length < 1) {
     return "The word is empty smh"; //check if player's turn
   }
   //challenge
   this.turnType = 1;
   this.turn = this.getPlayerIds().indexOf(this.lastPlayer.id);
-  return `${player.username} challenged ${this.lastPlayer.username}! They now have 5 seconds to send a word that starts with \`${this.letters}\`!`
+  return `${player.username} challenged ${this.lastPlayer.username}! They now have 10 seconds to send a word that starts with \`${this.letters}\`!`
 }
 
 Game.prototype.checkWord = function(allWords) {
@@ -80,8 +68,21 @@ Game.prototype.addPlayer = function(player) {
   this.players.push({
     id: player.id,
     username: player.username,
-    lives: 5
+    lives: 3
   })
+}
+
+Game.prototype.removeDeadPlayers = function() {
+  var deadPlayers = [];
+  for (var i = 0; i < this.players.length; i++) {
+    if (this.players[i].lives <= 0) {
+      deadPlayers.push(this.players[i]);
+      this.players = this.players.slice(0,i).concat(this.players.slice(i+1));
+      i--;
+      if (this.turn >= this.players.length) this.turn = 0; //fix turns after death
+    }
+  }
+  return deadPlayers;
 }
 
 Game.prototype.checkInGame = function(player) {
@@ -94,6 +95,7 @@ Game.prototype.checkInGame = function(player) {
 Game.prototype.nextTurn = function() {
   this.lastPlayer = this.players[this.turn];
   this.turn++;
+  this.totalTurns++;
   if (this.turn >= this.players.length) this.turn = 0;
 }
 
--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const game = require("./game.js");
 var fs = require("fs");
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]});
 
-var botPrefix = "l!";
+var botPrefix = "L!";
+
+//array of current games
 var games = [];
 
 //get word list
 var rawdata = fs.readFileSync('./words_alpha.txt', {encoding:'utf8', flag:'r'});
 var allWords = rawdata.split(/\r?\n/);
 
+//turn length
+var turnLength = 30; //in seconds
+var challengeLength = 10;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(botPrefix + "help"); 
 });
 
-//easter egg
-client.on('message', message => {
-    if (message.content.toLowerCase().search("think")!=-1){
-    message.react('🤔')
-            .catch(console.error);}
-});
 
 client.on('message', message => {
   //command prefix
@@ -40,12 +41,24 @@ client.on('message', message => {
 
     //commands
     switch (command) {
+      case "help"://help message
+        var Embed = new Discord.MessageEmbed()
+          .setColor("#87de90")
+          .setTitle("Help")
+          .setDescription("LetterPlay is a game all about letters!  Players each start with 3 lives and take turns.  Each turn, a player has two options: add a letter to a communal word or challenge the last move.  Each player must make their turn within "+turnLength+" seconds, or else they'll lose a life.  You're out once all your lives are gone, and the last player standing wins!")
+          .addField("Starting the game", "Start a new game with `" +botPrefix+ "start`, or join an existing game with `" + botPrefix + "join`.  You can only join a game before a player has lost a life.  Only one game can be played per channel!")
+          .addField("Adding a letter", "Just type a letter while it is your turn or use `" + botPrefix + "add <letter>`.  If the player makes a complete word when adding a letter, they lose a life and the word is reset.", true)
+          .addField("Challenging", "If a player can't think of a word with the given letters during their turn, they can challenge instead with `" + botPrefix + "challenge`.  In a challenge, the last player to make a move must respond complete the communal word to form a real word.  If a real word is made, the challenger loses a life, but if the challenged player cannot, they lose a life instead.", true)
+          .addField("Responding to a Challenge", "Just type a completed word once you are challenged or use `" + botPrefix + "respond <guess>`.  You gotta be quick, because you only get " + challengeLength + " seconds to respond to a challenge", true)
+          .addField("Other","Use `"+botPrefix+"lives` to view everyone's remaining lives.  This game uses a British English word list, so its colour not color!")
+        message.channel.send({embeds:[Embed]});
+        break;
       case "start"://starts a new game
         //check if player is already playing
         if (checkInGame(sender) == false){
           //check if there is already a game running
           if (g == -1) {
-            message.channel.send("New game started!  Type `"+botPrefix+"join` to join!")
+            msgBox(message, ":arrow_forward: New game started!", "Type `"+botPrefix+"join` to join!")
             games.push(_.cloneDeep(new game.Game(channel)));
             games[games.length-1].addPlayer(sender);
           }
@@ -54,28 +67,37 @@ client.on('message', message => {
           }
         }
         else {
-          message.channel.send("You are already playing!");
+          message.channel.send("You are already in a game!");
         }
         break;
       case "join":
         if (checkInGame(sender) == false){
           if (g !== -1) {
-            message.channel.send("You joined the game with "+ g.getPlayers())
+            //check first if anyone lost a life
+            var started = false;
+            for (var i = 0; i < g.players.length; i++) {
+              if (g.players[i].lives < 3) {
+                message.channel.send("Sorry!  This game has already started.  (A player has lost a life)");
+                started = true;
+                break;
+              }
+            }
+            //add player
+            if (!started) {
+              msgBox(message, ":arrow_forward: You joined the game!", "Playing with " + g.getPlayers().join(", "))
             g.addPlayer(sender);
+            }
           }
           else {
-            message.channel.send("There is no game to join here.");
+            message.channel.send("There is no game to join in this channel.  Use `" + botPrefix + "start` to start one!");
           }
         }
         else {
-          message.channel.send("You are already playing!");
+          message.channel.send("You are already in a game!");
         }
         break;
       case "add":
         addLetter(message, sender, channel);
-        break;
-      case "stop":
-        g.nextRound();
         break;
       case "lives":
         if (g == -1) break; //return if there is no game
@@ -83,21 +105,23 @@ client.on('message', message => {
         g.players.forEach((p)=>{
           response += p.username + ": " + p.lives + " lives\n"
         });
-        message.channel.send(response);
+        msgBox(message, "Lives left:", response);
         break;
       case "challenge":
         if (g == -1) break; //return if there is no game
+        if (!checkMove(message, sender, channel)) return; //return if invalid move
         var response = g.challenge(sender);
-        message.channel.send(response);
+        msgBox(message, ":bangbang: Challenge!", response);
+        //set timer for the challenged player
+        turnTimer(message, g.lastPlayer, channel, challengeLength);
         break;
       case "respond":
         respondChallenge(message, sender, channel);
         break;
-      case "embed":
-        var Embed = new Discord.MessageEmbed()
-        .setTitle("YO")
-        .setDescription("``TEXT GOES HERE``");
-        message.channel.send({embeds:[Embed]});
+      case "party":
+        message.channel.send(":tada: AYAYAYAYAYA :tada:").then((m) => {
+          m.react("🎉");
+        });
         break;
       default:
         message.channel.send(command + " is not a valid command");
@@ -113,9 +137,12 @@ client.on('message', message => {
       if (g.turnType == 1) respondChallenge(message, sender, channel);
     }
   }
+
+  //owo whats this
+  if (message.content.toLowerCase().search("think") != -1) message.react("🤔");
 });
 
-//utilities
+//utilities and functions
 function checkInGame(player) {
   for (var i = 0; i < games.length; i++) {
     if (games[i].checkInGame(player)) {
@@ -124,6 +151,7 @@ function checkInGame(player) {
   }
   return false;
 }
+
 function getGame(channelId) {
   for (var i = 0; i < games.length; i++) {
     if (channelId == games[i].channel) return games[i];
@@ -136,13 +164,17 @@ function addLetter(message, sender, channel) {
   var letter = msg.substring(msg.indexOf(" ")+1);
   var g = getGame(channel);
   if (g == -1) return; //return if there is no game
+  if (!checkMove(message, sender, channel)) return; //return if invalid move
   var response = g.addLetter(sender, letter);
-  message.channel.send(response);
+  msgBox(message, response);
   var isWord = g.checkWord(allWords);
   if (isWord) {
-    message.channel.send(`Round Over!  \`${g.letters}\` is a word.  ${g.lastPlayer.username} loses a life.  ${g.getPlayers()[g.turn]} gets to start the next round.`);
+    msgBox(message, ":octagonal_sign: Round Over - Word completed!", `\`${g.letters}\` is a word.  ${g.lastPlayer.username} loses a life.  ${g.getPlayers()[g.turn]} gets to start the next round.`);
     g.nextRound(g.lastPlayer);
+    removeDeadPlayers(message, channel);
   }
+  //start timer for next player's turn
+  turnTimer(message, g.players[g.turn], channel, turnLength);
 }
 
 function respondChallenge(message, sender, channel) {
@@ -150,14 +182,101 @@ function respondChallenge(message, sender, channel) {
   var guess = msg.substring(msg.indexOf(" ")+1);
   var g = getGame(channel);
   if (g == -1) return; //return if there is no game
+  if (!checkMove(message, sender, channel)) return; //return if invalid move
   var isWord = g.checkGuess(allWords, guess);
   if (isWord) { //challenge success
-    message.channel.send(`Round Over!  \`${guess}\` is a correct response to ${g.getPlayers()[g.turn]}'s challenge.  ${g.getPlayers()[g.turn]} loses a life, but gets to start the next round.`);
+    g.nextTurn();
+    msgBox(message, ":octagonal_sign: Round Over - Challenge failed!", `\`${guess}\` is a correct response to ${g.getPlayers()[g.turn]}'s challenge.  ${g.getPlayers()[g.turn]} loses a life and gets to start the next round.`);
     g.nextRound(g.players[g.turn]);
+    removeDeadPlayers(message, channel);
   } else { //challenge failure
-    message.channel.send(`Round Over!  \`${guess}\` is not a correct response to ${g.getPlayers()[g.turn]}'s challenge. ${g.lastPlayer.username} loses a life, and ${g.getPlayers()[g.turn]} gets to start the next round.`);
+    g.nextTurn();
+    msgBox(message, ":octagonal_sign: Round Over - Challenge succeeded!", `\`${guess}\` is not a correct response to ${g.getPlayers()[g.turn]}'s challenge. ${g.lastPlayer.username} loses a life, and ${g.getPlayers()[g.turn]} gets to start the next round.`);
     g.nextRound(g.lastPlayer);
+    removeDeadPlayers(message, channel);
   }
 }
 
-client.login('OTc0NzgyNjAxODkyMDgxNjg0.GRsoCE.Psj7Kyi_xcURKhGd1e74_tbZbcRZDqiwnyt1ps');
+function turnTimer(message, player, channel, time) {
+  //end turn
+  var g = getGame(channel);
+  var turn = g.turn;
+  var totalTurns = g.totalTurns;
+  setTimeout(() => {
+    if (getGame(channel) !== -1){//make sure game didn't end
+      if (g.totalTurns == totalTurns && g.turn == turn) {
+        g.nextTurn();
+        g.nextRound(g.lastPlayer);
+        removeDeadPlayers(message, channel);
+        msgBox(message, ":octagonal_sign: Round Over - Time limit!", `${g.lastPlayer.username} loses a life because they didn't take a turn in time.  ${g.getPlayers()[g.turn]} gets to start the next round.`);
+      }
+    }
+  },1000*time);
+  //warning message
+  setTimeout(() => {
+    if (getGame(channel) !== -1){//make sure game didn't end
+      if (g.totalTurns == totalTurns && g.turn == turn) {
+        message.channel.send(`Only ${time/2} seconds left in your turn, <@${player.id}>`)
+      }
+    }
+  },1000*time/2);
+}
+
+function checkMove(message, sender, channel) {
+  var g = getGame(channel);
+  if (!g.checkInGame(sender)) {
+    message.channel.send("You aren't playing in this game.  Type `"+botPrefix+"join` to join!"); //check if not in game
+    return false;
+  }
+  else if (g.getPlayerIds()[g.turn] !== sender.id) {
+    message.channel.send("It is " + g.getPlayers()[g.turn]+"'s turn now."); //check if player's turn
+    return false;
+  }
+  else if (g.players.length < 2) {
+    message.channel.send("You can't play by yourself!  Find someone to type `"+botPrefix+"join` to join!"); //check if only player
+    return false;
+  }
+  return true;
+}
+
+function removeDeadPlayers(message, channel) {
+  var g = getGame(channel);
+  var deadPlayers = g.removeDeadPlayers();
+  var emojis = ["🥇","🥈","🥉"];//medal reactions to top finishers
+  for (var i = 0; i < deadPlayers.length; i++) {
+    msgBox(message, ":skull: Player out!", deadPlayers[i].username + " ran out of lives!").then((m) => {
+      var playersLeft = g.players.length;
+      if (emojis[playersLeft]) m.react(emojis[playersLeft]);
+    });
+  }
+  //end game
+  if (g.players.length == 1) {
+    endGame(message, channel, g.players[0]);
+  }
+}
+
+function endGame(message, channel, winner) {
+  var g = getGame(channel);
+  msgBox(message, ":trophy: VICTORY! :trophy:", `<@${winner.id}> won with ${winner.lives} lives left!`).then((m) => {
+    m.react("🎉");
+  });
+  //delete the game instance
+  for (var i = 0; i < games.length; i++) {
+    if (g.channel == games[i].channel) {
+      games = games.slice(0,i).concat(games.slice(i+1));
+      break;
+    }
+  }
+}
+
+function msgBox(message, title, text) {
+  if (!title) title = "";
+  if (!text) text = "";
+  var Embed = new Discord.MessageEmbed()
+        .setColor("#87de90")
+        .setTitle(title)
+        .setDescription(text);
+  return message.channel.send({embeds:[Embed]});
+}
+
+client.login("OTc0NzgyNjAxODkyMDgxNjg0.GRsoCE.Psj7Kyi_xcURKhGd1e74_tbZbcRZDqiwnyt1ps");
